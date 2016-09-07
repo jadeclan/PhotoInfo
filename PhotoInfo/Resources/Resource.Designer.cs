357b4b028d626a478b83b4f73680b133db3be567
James Bergeron <jberg475@mtroyal.ca>
2016-09-06 20:32:30 -0600

    MainScreen Background Animation

--- PhotoInfo/Resources/Resource.Designer.cs
+++ PhotoInfo/Resources/Resource.Designer.cs
@@ -518,6 +518,9 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f010090
 			public const int ratingBarStyleSmall = 2130772112;
 			
+			// aapt resource value: 0x7f0100d1
+			public const int scrollingDrawable = 2130772177;
+			
 			// aapt resource value: 0x7f0100b0
 			public const int searchHintIcon = 2130772144;
 			
@@ -1452,17 +1455,23 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f02004c
 			public const int Icon = 2130837580;
 			
-			// aapt resource value: 0x7f020050
-			public const int notification_template_icon_bg = 2130837584;
-			
 			// aapt resource value: 0x7f02004d
-			public const int photoInfoIcon = 2130837581;
+			public const int main = 2130837581;
 			
 			// aapt resource value: 0x7f02004e
-			public const int scrollbar_vertical_thumb = 2130837582;
+			public const int main_background = 2130837582;
+			
+			// aapt resource value: 0x7f020052
+			public const int notification_template_icon_bg = 2130837586;
 			
 			// aapt resource value: 0x7f02004f
-			public const int scrollbar_vertical_track = 2130837583;
+			public const int photoInfoIcon = 2130837583;
+			
+			// aapt resource value: 0x7f020050
+			public const int scrollbar_vertical_thumb = 2130837584;
+			
+			// aapt resource value: 0x7f020051
+			public const int scrollbar_vertical_track = 2130837585;
 			
 			static Drawable()
 			{
@@ -1477,8 +1486,8 @@ namespace PhotoInfo
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0b0051
-			public const int action0 = 2131427409;
+			// aapt resource value: 0x7f0b0053
+			public const int action0 = 2131427411;
 			
 			// aapt resource value: 0x7f0b0041
 			public const int action_bar = 2131427393;
@@ -1504,8 +1513,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0042
 			public const int action_context_bar = 2131427394;
 			
-			// aapt resource value: 0x7f0b0055
-			public const int action_divider = 2131427413;
+			// aapt resource value: 0x7f0b0057
+			public const int action_divider = 2131427415;
 			
 			// aapt resource value: 0x7f0b0002
 			public const int action_menu_divider = 2131427330;
@@ -1537,14 +1546,14 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b002b
 			public const int buttonPanel = 2131427371;
 			
-			// aapt resource value: 0x7f0b0052
-			public const int cancel_action = 2131427410;
+			// aapt resource value: 0x7f0b0054
+			public const int cancel_action = 2131427412;
 			
 			// aapt resource value: 0x7f0b0039
 			public const int checkbox = 2131427385;
 			
-			// aapt resource value: 0x7f0b0058
-			public const int chronometer = 2131427416;
+			// aapt resource value: 0x7f0b005a
+			public const int chronometer = 2131427418;
 			
 			// aapt resource value: 0x7f0b001d
 			public const int collapseActionView = 2131427357;
@@ -1558,8 +1567,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0036
 			public const int customPanel = 2131427382;
 			
-			// aapt resource value: 0x7f0b0060
-			public const int dataTextView = 2131427424;
+			// aapt resource value: 0x7f0b0062
+			public const int dataTextView = 2131427426;
 			
 			// aapt resource value: 0x7f0b003f
 			public const int decor_content_parent = 2131427391;
@@ -1576,8 +1585,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b001a
 			public const int end = 2131427354;
 			
-			// aapt resource value: 0x7f0b005d
-			public const int end_padder = 2131427421;
+			// aapt resource value: 0x7f0b005f
+			public const int end_padder = 2131427423;
 			
 			// aapt resource value: 0x7f0b0025
 			public const int expand_activities_button = 2131427365;
@@ -1585,8 +1594,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0038
 			public const int expanded_menu = 2131427384;
 			
-			// aapt resource value: 0x7f0b0050
-			public const int getImageBtn = 2131427408;
+			// aapt resource value: 0x7f0b0052
+			public const int getImageBtn = 2131427410;
 			
 			// aapt resource value: 0x7f0b0004
 			public const int home = 2131427332;
@@ -1603,14 +1612,14 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0026
 			public const int image = 2131427366;
 			
+			// aapt resource value: 0x7f0b005e
+			public const int info = 2131427422;
+			
+			// aapt resource value: 0x7f0b0058
+			public const int line1 = 2131427416;
+			
 			// aapt resource value: 0x7f0b005c
-			public const int info = 2131427420;
-			
-			// aapt resource value: 0x7f0b0056
-			public const int line1 = 2131427414;
-			
-			// aapt resource value: 0x7f0b005a
-			public const int line3 = 2131427418;
+			public const int line3 = 2131427420;
 			
 			// aapt resource value: 0x7f0b0009
 			public const int listMode = 2131427337;
@@ -1618,8 +1627,14 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0028
 			public const int list_item = 2131427368;
 			
-			// aapt resource value: 0x7f0b0054
-			public const int media_actions = 2131427412;
+			// aapt resource value: 0x7f0b0050
+			public const int main_background = 2131427408;
+			
+			// aapt resource value: 0x7f0b0051
+			public const int main_content = 2131427409;
+			
+			// aapt resource value: 0x7f0b0056
+			public const int media_actions = 2131427414;
 			
 			// aapt resource value: 0x7f0b001b
 			public const int middle = 2131427355;
@@ -1660,8 +1675,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0033
 			public const int scrollView = 2131427379;
 			
-			// aapt resource value: 0x7f0b005f
-			public const int scrollView1 = 2131427423;
+			// aapt resource value: 0x7f0b0061
+			public const int scrollView1 = 2131427425;
 			
 			// aapt resource value: 0x7f0b0045
 			public const int search_badge = 2131427397;
@@ -1696,8 +1711,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b004f
 			public const int select_dialog_listview = 2131427407;
 			
-			// aapt resource value: 0x7f0b005e
-			public const int selectedImageView = 2131427422;
+			// aapt resource value: 0x7f0b0060
+			public const int selectedImageView = 2131427424;
 			
 			// aapt resource value: 0x7f0b003a
 			public const int shortcut = 2131427386;
@@ -1726,8 +1741,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b0018
 			public const int src_over = 2131427352;
 			
-			// aapt resource value: 0x7f0b0053
-			public const int status_bar_latest_event_content = 2131427411;
+			// aapt resource value: 0x7f0b0055
+			public const int status_bar_latest_event_content = 2131427413;
 			
 			// aapt resource value: 0x7f0b004c
 			public const int submit_area = 2131427404;
@@ -1735,20 +1750,20 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b000b
 			public const int tabMode = 2131427339;
 			
-			// aapt resource value: 0x7f0b005b
-			public const int text = 2131427419;
+			// aapt resource value: 0x7f0b005d
+			public const int text = 2131427421;
 			
-			// aapt resource value: 0x7f0b0059
-			public const int text2 = 2131427417;
+			// aapt resource value: 0x7f0b005b
+			public const int text2 = 2131427419;
 			
 			// aapt resource value: 0x7f0b0034
 			public const int textSpacerNoButtons = 2131427380;
 			
-			// aapt resource value: 0x7f0b0062
-			public const int textView1 = 2131427426;
+			// aapt resource value: 0x7f0b0064
+			public const int textView1 = 2131427428;
 			
-			// aapt resource value: 0x7f0b0057
-			public const int time = 2131427415;
+			// aapt resource value: 0x7f0b0059
+			public const int time = 2131427417;
 			
 			// aapt resource value: 0x7f0b002a
 			public const int title = 2131427370;
@@ -1756,8 +1771,8 @@ namespace PhotoInfo
 			// aapt resource value: 0x7f0b002f
 			public const int title_template = 2131427375;
 			
-			// aapt resource value: 0x7f0b0061
-			public const int toolbar = 2131427425;
+			// aapt resource value: 0x7f0b0063
+			public const int toolbar = 2131427427;
 			
 			// aapt resource value: 0x7f0b002e
 			public const int topPanel = 2131427374;
@@ -3927,6 +3942,12 @@ namespace PhotoInfo
 			
 			// aapt resource value: 0
 			public const int PopupWindowBackgroundState_state_above_anchor = 0;
+			
+			public static int[] ScrollingView = new int[] {
+					2130772177};
+			
+			// aapt resource value: 0
+			public const int ScrollingView_scrollingDrawable = 0;
 			
 			public static int[] SearchView = new int[] {
 					16842970,
